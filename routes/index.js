051f98c23d6143ca202353a202a58a43c22e65ef
Lpik97 <dm97fx@gmail.com>
2024-06-30 10:21:29 +0200

    feat(routes): handle form submissions to add new messages
    - Added a POST route to handle form submissions on the "/new" endpoint.
    - The new route captures user input and updates the messages array.
    - Redirects to the home page after a new message is added.
    
    This enables users to add new messages through the form.

--- routes/index.js
+++ routes/index.js
@@ -23,4 +23,14 @@ router.get("/new", (req, res, next) => {
    res.render("form", { title: "Add a new message" });
 });
 
+router.post("/new", (req, res, next) => {
+   const message = {
+      text: req.body.text,
+      user: req.body.user,
+      added: new Date(),
+   };
+   messages.push(message);
+   res.redirect("/");
+});
+
 export default router;
